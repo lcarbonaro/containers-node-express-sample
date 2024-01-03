28bebc63f8ed82e1146b1acbfbc02d3eb7c3a23d
Les Carbonaro <lcarbonaro@gmail.com>
2024-01-03 15:07:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send(`
-    JUST TESTING
-  `);
+  res.send(`JUST TESTING`);
 });
 
 app.get('/test', (req, res) => {
